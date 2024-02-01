9a994316be98c88a77807add7c7bc515c2ddc951
Papa Fall <papa.fall1@gmail.com>
2024-01-31 23:18:38 -0800

    Added function to determine game winner

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// Function to randomize the computer selection
-function getComputerChoice() {
-  const rps = ["rock", "paper", "scissors"];
-  const choice = rps[Math.floor(Math.random() * rps.length)];
-  return choice;
-}
-
+// Getting the DOMs
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
@@ -15,11 +9,17 @@ const gameMessage = document.querySelector("#game-message");
 gameMessage.appendChild(selections);
 gameMessage.appendChild(winningMessage);
 
-const overallScore = document.querySelector("#overallScore");
+const playerTotal = document.querySelector("#playerTotal");
+const computerTotal = document.querySelector("#computerTotal");
 
-let playerChoice;
-let computerChoice;
+// Function to randomize the computer selection
+function getComputerChoice() {
+  const rps = ["rock", "paper", "scissors"];
+  const choice = rps[Math.floor(Math.random() * rps.length)];
+  return choice;
+}
 
+//Defining the scores
 let playerScore = 0;
 let computerScore = 0;
 let winningScore = 5;
@@ -27,81 +27,66 @@ let winningScore = 5;
 //Function to deterine winner for 1 round
 function rpsRound(playerChoice, computerChoice) {
   //const computerChoice = getComputerChoice();
-  selections.textContent = `Computer chose ${computerChoice}`;
-  let roundWinner = "";
+  selections.textContent = ` You chose ${playerChoice} -- Computer chose ${computerChoice}`;
 
   if (playerChoice === computerChoice) {
-    roundWinner = `It's a tie! You both selected ${computerChoice}`;
-    winningMessage.textContent = roundWinner;
+    return `It's a tie! You both selected ${computerChoice}`;
   } else if (
     (playerChoice === "rock" && computerChoice === "scissors") ||
     (playerChoice === "paper" && computerChoice === "rock") ||
     (playerChoice === "scissors" && computerChoice === "paper")
   ) {
-    roundWinner = `🏆 You win! ${playerChoice} beats ${computerChoice}`;
-    winningMessage.textContent = roundWinner;
-    playerScore++;
+    return `🏆 You win! ${playerChoice} beats ${computerChoice}`;
   } else {
-    roundWinner = `You Lose! ${computerChoice} beats ${playerChoice}`;
-    winningMessage.textContent = roundWinner;
+    return `You Lose! ${computerChoice} beats ${playerChoice}`;
+  }
+}
+
+//Function to announce game winner; first player to reach 5 points
+function gameWinner() {
+  if (playerScore === winningScore || computerScore === winningScore) {
+    if (playerScore > computerScore) {
+      alert("Congratulations! You win the game! 🏆");
+    } else if (computerScore > playerScore) {
+      alert("Game over - you lose 😔");
+    }
+    location.reload();
+  }
+}
+
+//Function to play 1 game
+function game() {
+  const round = rpsRound(playerChoice, computerChoice);
+
+  if (round === `🏆 You win! ${playerChoice} beats ${computerChoice}`) {
+    playerScore++;
+  } else if (round === `You Lose! ${computerChoice} beats ${playerChoice}`) {
     computerScore++;
   }
+  winningMessage.textContent = round;
+  playerTotal.textContent = playerScore;
+  computerTotal.textContent = computerScore;
+
+  gameWinner();
 }
 
 rock.addEventListener("click", function () {
   playerChoice = "rock";
   computerChoice = getComputerChoice();
 
-  rpsRound(playerChoice, computerChoice);
+  game();
 });
 
 paper.addEventListener("click", function () {
   playerChoice = "paper";
   computerChoice = getComputerChoice();
 
-  rpsRound(playerChoice, computerChoice);
+  game();
 });
 
 scissors.addEventListener("click", function () {
   playerChoice = "scissors";
   computerChoice = getComputerChoice();
 
-  rpsRound(playerChoice, computerChoice);
+  game();
 });
-
-//Game function that loops and adds 1 point to overall score until someone reaches 5 points and wins the game
-/*
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  let winningScore = 5;
-
-  while (playerScore < winningScore && computerScore < winningScore) {
-    //const playerChoice = prompt("rock, paper, or scissors?").toLowerCase();
-    //const computerChoice = getComputerChoice();
-    console.log(`You chose: ${playerChoice}`);
-    console.log(`Computer chose: ${computerChoice}`);
-
-    const roundResult = rpsRound(playerChoice, computerChoice);
-    console.log(roundResult);
-
-    roundResult;
-
-    if (roundResult === `🏆 You win! ${playerChoice} beats ${computerChoice}`) {
-      playerScore++;
-    } else if (
-      roundResult === `You Lose! ${computerChoice} beats ${playerChoice}`
-    ) {
-      computerScore++;
-    }
-
-    console.log(`Score - You: ${playerScore} | Computer: ${computerScore}`);
-  }
-
-  if (playerScore === winningScore) {
-    console.log(`Congrats! You win the game! 🏆`);
-  } else if (computerScore === winningScore) {
-    console.log(`whelp... You lost, boy. Better luck next time`);
-  }
-}
-*/
