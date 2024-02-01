35b045f45b3c88afff60d9e82ddeec314f0cbdaf
Papa Fall <papa.fall1@gmail.com>
2024-01-31 22:13:32 -0800

    Added event handler for all 3 buttons

--- script.js
+++ script.js
@@ -5,30 +5,80 @@ function getComputerChoice() {
   return choice;
 }
 
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+const selections = document.createElement("p");
+const winningMessage = document.createElement("p");
+const gameMessage = document.querySelector("#game-message");
+gameMessage.appendChild(selections);
+gameMessage.appendChild(winningMessage);
+
+const overallScore = document.querySelector("#overallScore");
+
+let playerChoice;
+let computerChoice;
+
+let playerScore = 0;
+let computerScore = 0;
+let winningScore = 5;
+
 //Function to deterine winner for 1 round
 function rpsRound(playerChoice, computerChoice) {
+  //const computerChoice = getComputerChoice();
+  selections.textContent = `Computer chose ${computerChoice}`;
+  let roundWinner = "";
+
   if (playerChoice === computerChoice) {
-    return `It's a tie! You both selected ${computerChoice}`;
+    roundWinner = `It's a tie! You both selected ${computerChoice}`;
+    winningMessage.textContent = roundWinner;
   } else if (
     (playerChoice === "rock" && computerChoice === "scissors") ||
     (playerChoice === "paper" && computerChoice === "rock") ||
     (playerChoice === "scissors" && computerChoice === "paper")
   ) {
-    return `🏆 You win! ${playerChoice} beats ${computerChoice}`;
+    roundWinner = `🏆 You win! ${playerChoice} beats ${computerChoice}`;
+    winningMessage.textContent = roundWinner;
+    playerScore++;
   } else {
-    return `You Lose! ${computerChoice} beats ${playerChoice}`;
+    roundWinner = `You Lose! ${computerChoice} beats ${playerChoice}`;
+    winningMessage.textContent = roundWinner;
+    computerScore++;
   }
 }
 
+rock.addEventListener("click", function () {
+  playerChoice = "rock";
+  computerChoice = getComputerChoice();
+
+  rpsRound(playerChoice, computerChoice);
+});
+
+paper.addEventListener("click", function () {
+  playerChoice = "paper";
+  computerChoice = getComputerChoice();
+
+  rpsRound(playerChoice, computerChoice);
+});
+
+scissors.addEventListener("click", function () {
+  playerChoice = "scissors";
+  computerChoice = getComputerChoice();
+
+  rpsRound(playerChoice, computerChoice);
+});
+
 //Game function that loops and adds 1 point to overall score until someone reaches 5 points and wins the game
+/*
 function game() {
   let playerScore = 0;
   let computerScore = 0;
   let winningScore = 5;
 
   while (playerScore < winningScore && computerScore < winningScore) {
-    const playerChoice = prompt("rock, paper, or scissors?").toLowerCase();
-    const computerChoice = getComputerChoice();
+    //const playerChoice = prompt("rock, paper, or scissors?").toLowerCase();
+    //const computerChoice = getComputerChoice();
     console.log(`You chose: ${playerChoice}`);
     console.log(`Computer chose: ${computerChoice}`);
 
@@ -54,5 +104,4 @@ function game() {
     console.log(`whelp... You lost, boy. Better luck next time`);
   }
 }
-
-game();
+*/
